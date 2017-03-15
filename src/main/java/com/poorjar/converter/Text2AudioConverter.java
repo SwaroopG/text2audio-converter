910a300b2a2e3cb0275b4f996c10ddb8d6ff586e
csdavidson09 <csdavidson09@gmail.com>
2017-03-14 21:05:44 -0500

    Use ffmpeg to finish wav to mp3 conversion

--- src/main/java/com/poorjar/converter/Text2AudioConverter.java
+++ src/main/java/com/poorjar/converter/Text2AudioConverter.java
@@ -3,9 +3,11 @@ package com.poorjar.converter;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Set;
 import java.util.StringTokenizer;
 
+import com.google.common.base.Stopwatch;
 import marytts.client.MaryClient;
 import marytts.util.http.Address;
 
@@ -29,8 +31,7 @@ import com.google.common.collect.Sets;
  * @see <a href="https://github.com/marytts/marytts/releases/tag/v4.3.1">https://github.com/marytts/marytts/releases/tag/v4.3.1</a> <p/>
  * @author Swaroop
  */
-public class Text2AudioConverter
-{
+public class Text2AudioConverter {
     static final Logger LOGGER = Logger.getLogger(Text2AudioConverter.class);
 
     private static String maryHostname = "localhost";
@@ -42,42 +43,40 @@ public class Text2AudioConverter
     private static String audioType = "WAVE";
     private static String voiceName = "cmu-slt-hsmm";
 
+    private String outputPath = "";
+
     /**
      * Converts a given text file into a audio file of WAVE format.
      * @param inputFile The input file path. Cannot be null or empty.
      * @throws Exception
      */
-    public void convertText2Audio(String inputFile) throws Exception
-    {
-        // Verify the file name
-        if (StringUtils.isEmpty(inputFile))
-        {
-            throw new IllegalArgumentException("Invalid file path");
+    public void convertText2Audio(String inputFile) throws IOException {
+        LOGGER.debug("Starting conversion of file: " + inputFile);
+        if (StringUtils.isEmpty(inputFile)) {
+            throw new IllegalArgumentException("Invalid file path for file: " + inputFile);
         }
+        StringTokenizer tokenizer = new StringTokenizer(inputFile, ".");
+        outputPath = tokenizer.nextToken();
 
-        LOGGER.debug("Starting conversion of file : " + inputFile);
+        String inputText = readFileToString(inputFile);
+        convertToWav(inputText, outputPath);
+        WaveToMp3Converter.convertWavToMp3(outputPath);
+    }
 
-        // Timing the conversion process
-        long startTime = System.nanoTime();
+    // TODO: This is very memory inefficient.
+    private static String readFileToString(String inputFile) throws IOException {
+        return FileUtils.readFileToString(new File(inputFile));
+    }
 
-        // TODO: This is very memory inefficient.
-        String inputText = FileUtils.readFileToString(new File(inputFile));
-
-        // Text to Audio format conversion
+    private static void convertToWav(String inputText, String outputPath) throws IOException {
         ByteArrayOutputStream audioOutputStream = new ByteArrayOutputStream();
         MaryClient mary = MaryClient.getMaryClient(new Address(maryHostname, maryPort));
+        //TODO voiceName throws error if voice is not installe I only had cmu-slt locally
         mary.process(inputText, inputType, outputType, locale, audioType, voiceName, audioOutputStream);
-
-        // The byte array constitutes a full wave file, including the header. Write the byte array to file.
-        FileOutputStream fileOut = new FileOutputStream(getOutputFile(inputFile));
+        FileOutputStream fileOut = new FileOutputStream(new File(outputPath + ".wav"));
         audioOutputStream.writeTo(fileOut);
         audioOutputStream.flush();
         fileOut.close();
-
-        // Timing purpose
-        long endTime = System.nanoTime();
-        long duration = (endTime - startTime);
-        LOGGER.info("Conversion time for file : " + inputFile + " took " + duration / 1000000000.0 + " secs");
     }
 
     /**
@@ -86,17 +85,16 @@ public class Text2AudioConverter
      * @param directory The directory path. Cannot be null or empty.
      * @throws Exception
      */
-    public void convertText2AudioFromDir(String directory) throws Exception
-    {
+    public void convertText2AudioFromDir(String directory) throws Exception {
         // Verify the directory name
-        if (StringUtils.isEmpty(directory))
-        {
+        if (StringUtils.isEmpty(directory)) {
             throw new IllegalArgumentException("Invalid directory path");
         }
 
-        for (String inputFile : getFiles(directory))
-        {
+        for (String inputFile : getFiles(directory)) {
+            Stopwatch timer = Stopwatch.createStarted();
             convertText2Audio(inputFile);
+            LOGGER.info("Conversion time for file - " + inputFile + " - took " + timer.stop());
         }
     }
 
@@ -107,23 +105,13 @@ public class Text2AudioConverter
      * @param directory
      * @return A set of file names.
      */
-    private Set<String> getFiles(String directoryPath)
-    {
-        try
-        {
+    private Set<String> getFiles(String directoryPath) {
+        try {
             return Sets.newHashSet(marytts.util.io.FileUtils.getFileList(directoryPath, "txt"));
         }
-        catch (NullPointerException e)
-        {
+        catch (NullPointerException e) {
             throw new IllegalArgumentException("Invalid directory path : " + directoryPath);
         }
-
-    }
-
-    private File getOutputFile(String fileName)
-    {
-        StringTokenizer tokenizer = new StringTokenizer(fileName, ".");
-        return new File(tokenizer.nextToken() + ".wav");
     }
 
     // TODO: Experiment with Codahale Metrics later
