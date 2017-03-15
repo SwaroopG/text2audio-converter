8f2f2e103d8f8912461462593c043dc4fcdea4ba
csdavidson09 <csdavidson09@gmail.com>
2017-03-14 21:05:44 -0500

    Use ffmpeg to finish wav to mp3 conversion

--- README.md
+++ README.md
@@ -12,11 +12,14 @@ MARY TTS Server should be running locally.
 * Step 1: Download the MARY TTS Standalone server
 * Step 2: Start the MARY TTS Server at (installDir)/bin/maryserver.
 * Step 3: Verify the MARY TTS Server status at http://localhost:59125/
+* Step 4: Download FFmpeg (tested on 3.2.4)
+* Step 5: Update WaveToMp3Converter.pathToFFmpegBin to the bin path where you installed FFmpeg
  
 ## Links
 * http://mary.dfki.de/download/index.html
 * https://github.com/marytts/marytts/releases/tag/v4.3.1
 * [Mary TTS API Parameters details](http://blog.bickle.co.uk/it-technology/maryspeak-a-command-line-wrapper-for-marytts)
+* https://ffmpeg.zeranoe.com/builds/
 
 [1]: https://secure.travis-ci.org/SwaroopG/text2audio-converter.png
 [2]: http://www.travis-ci.org/SwaroopG/text2audio-converter
--- src/main/java/com/poorjar/converter/WaveToMp3Converter.java
+++ src/main/java/com/poorjar/converter/WaveToMp3Converter.java
@@ -1,18 +1,39 @@
 package com.poorjar.converter;
 
+import net.bramp.ffmpeg.FFmpeg;
+import net.bramp.ffmpeg.FFmpegExecutor;
+import net.bramp.ffmpeg.FFprobe;
+import net.bramp.ffmpeg.builder.FFmpegBuilder;
+import net.bramp.ffmpeg.job.FFmpegJob;
+import net.bramp.ffmpeg.progress.Progress;
+import net.bramp.ffmpeg.progress.ProgressListener;
+
 import java.io.File;
-import java.util.StringTokenizer;
+import java.io.IOException;
 
-public class WaveToMp3Converter
-{
-    public void convertWavetoMp3(String filePath)
-    {
+public class WaveToMp3Converter {
 
-    }
+    private static String pathToFFmpegBin = "C:\\Users\\csdavidson\\projects\\text2audio-converter\\ffmpeg-20170312-58f0bbc-win64-static\\bin\\";
 
-    private File getOutputFile(String fileName)
-    {
-        StringTokenizer tokenizer = new StringTokenizer(fileName, ".");
-        return new File(tokenizer.nextToken() + ".wav");
+    public static void convertWavToMp3(final String filePath) throws IOException {
+        FFmpeg ffmpeg = new FFmpeg(pathToFFmpegBin + "ffmpeg.exe");
+        FFprobe ffprobe = new FFprobe(pathToFFmpegBin + "ffprobe.exe");
+        FFmpegBuilder builder = new FFmpegBuilder()
+                .setInput(filePath + ".wav")
+                .overrideOutputFiles(true)
+                .addOutput(filePath + ".mp3")
+                .setFormat("mp3")
+                .done();
+        FFmpegExecutor executor = new FFmpegExecutor(ffmpeg, ffprobe);
+        FFmpegJob job = executor.createJob(builder, new ProgressListener() {
+            @Override
+            public void progress(Progress progress) {
+                if (progress.isEnd()) {
+                    File oldWav = new File(filePath + ".wav");
+                    oldWav.delete();
+                }
+            }
+        });
+        job.run();
     }
 }
